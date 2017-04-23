83657b2b0de72314cdf11aa3468591629257be4b
az <1457800393@qq.com>
2017-04-23 19:55:04 +0800

    4/23

---
+++ public/javascripts/firstPage.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',function(){    
+     var oDiv = $(".zhj_content").children('div').length
+     console.log(oDiv)
+     		
+     
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+},false)
