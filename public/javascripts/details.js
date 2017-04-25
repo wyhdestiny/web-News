0cf9caaa3e31ef8c55dd2d8155b5fb828746dc83
Summer <1536904509@qq.com>
2017-04-25 19:23:33 +0800

    34

--- public/javascripts/details.js
+++ public/javascripts/details.js
@@ -1,14 +1,21 @@
 window.addEventListener("load",function(){
 	
-	
-//	var str = string.split(str)[0]; 
+	var html='';
+  	var id =  location.href.split('id')[1];
+   console.log(id);
 	$.ajax({
 		type:"post",
 		url:"http://192.168.43.4:3000/newlist/clicks",
 		async:true,
 		data:{
-//			newid:
+			newid:id
+		},
+		success:function(e){
+			console.log(e.data[0]);
+			html+='<div class="txt_tit">'+e.data[0].title+'</div><div class="yu_autor">文&nbsp;/&nbsp;F.S菲茨杰拉德</div><div class="yu_cont" > '+e.data[0].content+'</div>'
+			$(".yu_debody").append(html);
 		}
+		
 	});
 	
 	
