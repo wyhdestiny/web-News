1b074cb19234ec430575a7e75cce6d26373075a0
lhq17600882662 <2665309500@qq.com>
2017-04-26 10:12:19 +0800

    lhq426

--- public/javascripts/personMsg.js
+++ public/javascripts/personMsg.js
@@ -16,6 +16,8 @@ window.addEventListener("load",function(){
 
 
 //获取个人信息
+var lhqarr=[];
+var lhqarr2 = [];
 	$.ajax({
 		type:"get",
 		url:"http://192.168.43.4:3000/user/change",
@@ -27,14 +29,25 @@ window.addEventListener("load",function(){
 			sessionStorage.img=data.data[0].img;
 			console.log(sessionStorage.img)
 			$('.lhq-touxiang').attr('src',data.data[0].img);
-			$('.lhq-nc').text(data.data[0].username)
+			$('.lhq-nc').text(data.data[0].username);
+			
+			var lhqsss = data.data[0].releases;
+			lhqarr=lhqsss.split('-');
+			for(var i = 0; i<lhqarr.length;i++){
+				if(lhqarr[i]!='null'){
+					lhqarr2.push(lhqarr[i])
+				}
+			}
+			$('.lhqff').text(lhqarr2.length)
 		}
 	});
 	
+	
+
 	var lhqsb = sessionStorage.level;
 	console.log(lhqsb)
 	
-	if(lhqsb==1){
+	if(lhqsb==0){
 		$('.lhqdsh').css('display','none')
 	}
 	
